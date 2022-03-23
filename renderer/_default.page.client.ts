c5eb32a1e017a7c8f244b3864c347796e9cea07a
zRain <2296342883@qq.com>
2022-03-23 21:02:10 +0800

    perf: optimize tsconfig & test prod commond

--- renderer/_default.page.client.ts
+++ renderer/_default.page.client.ts
@@ -1,13 +1,10 @@
 import { createApp } from './app'
 import { useClientRouter } from 'vite-plugin-ssr/client/router'
 import type { PageContextBuiltInClient } from 'vite-plugin-ssr/client/router'
-import type { App } from 'vue'
 
 let AppInstance: ReturnType<typeof createApp> | null = null
 const { hydrationPromise } = useClientRouter({
   render(pageContext: PageContextBuiltInClient & PageContext) {
-    console.log(pageContext);
-    
     if (!AppInstance) {
       AppInstance = createApp(pageContext)
       AppInstance.mount('#app')
@@ -15,7 +12,6 @@ const { hydrationPromise } = useClientRouter({
       AppInstance.changePage(pageContext)
     }
   },
-  // Vue needs the first render to be a hydration
   ensureHydration: true,
   prefetchLinks: true,
   onTransitionStart,
