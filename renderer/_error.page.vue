cd8f7d499895347620113f6fe226a06c117767e3
zRain <2296342883@qq.com>
2022-05-14 23:46:53 +0800

    feat: 🎸 new error page

--- renderer/_error.page.vue
+++ renderer/_error.page.vue
@@ -1,11 +1,20 @@
 <template>
-  <div v-if="is404">
-    <h1>404 Page Not Found</h1>
-    <p>This page could not be found.</p>
-  </div>
-  <div v-else>
-    <h1>500 Internal Server Error</h1>
-    <p>Something went wrong.</p>
+  <div class="errorPage">
+    <div v-if="is404" class="code-area">
+      <span style="color: #7f848e; font-style: italic"> // {{ is404 ? '404 Page Not Found' : '500 Internal Server Error' }} </span>
+      <span>
+        <span style="color: #c678d2"> if </span>
+        (<span style="color: #4ca8ef">!</span><span style="font-style: italic; color: #e05c5a">found</span>) {
+      </span>
+      <span>
+        <span style="padding-left: 15px; color: #c678d2"> <i style="width: 10px; display: inline-block"></i>throw new </span>
+        <span style="color: #ffc83d">Error</span>
+        <span>(<span style="color: #67b779">"(╯°□°)╯︵ ┻━┻"</span>); </span>
+        <span style="display: block">}</span>
+        <br />
+        <span style="color: #7f848e; font-style: italic"> // <a href="/">Go home!</a> </span>
+      </span>
+    </div>
   </div>
 </template>
 
@@ -18,3 +27,41 @@ defineProps({
   }
 })
 </script>
+
+<style lang="scss">
+@import './styles/colors.scss';
+
+.errorPage {
+  display: flex;
+  justify-content: center;
+  .code-area {
+    line-height: 25px;
+    width: 380px;
+    min-width: 380px;
+  }
+
+  .code-area > span {
+    display: block;
+  }
+
+  a {
+    color: inherit;
+    &:hover {
+      color: $primary;
+      text-decoration: underline;
+    }
+  }
+}
+
+@media screen and (max-width: 320px) {
+  .code-area {
+    font-size: 5vw;
+    min-width: auto;
+    width: 95%;
+    margin: auto;
+    padding: 5px;
+    padding-left: 10px;
+    line-height: 6.5vw;
+  }
+}
+</style>
