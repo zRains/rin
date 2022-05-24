48bc5772cc07954e74a9b99825d9c3f37da05b8b
zRain <2296342883@qq.com>
2022-05-24 19:25:58 +0800

    feat: 🎸 new friends link

--- utils/constants.ts
+++ utils/constants.ts
@@ -44,5 +44,12 @@ export const sponsors = [
     avatar: 'https://www.deelter.com/images/logo.png',
     link: 'https://www.deelter.com',
     desc: '这个人很懒，什么也没留下...'
+  },
+  {
+    range: 2,
+    name: 'fzs',
+    avatar: 'http://www.zlight.club/img/weblog.74d55116.jpg',
+    link: 'http://www.zlight.club/',
+    desc: 'Nothing...'
   }
 ]
